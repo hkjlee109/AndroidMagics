4c884977e097016e3de1974367270b6c6b2aa858
kwoolytech <kwooly.tech@gmail.com>
2016-09-04 17:23:27 +0900

    Hello, AndroidMagic.

---
+++ app/src/main/java/com/kwoolytech/androidmagics/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kwoolytech.androidmagics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
